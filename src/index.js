e80de96d886722f4b8a1ef6b39e72cf750b5f1c0
CPM <cpm.chuck@gmail.com>
2017-09-20 01:19:19 +0800

    project init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <h1>project init</h1>
+}
+
+ReactDOM.render(<App />, window.document.getElementById("app"));
