148e6fb4867e6025e681cc7de8c9f4bc0f23f0df
CPM <cpm.chuck@gmail.com>
2017-09-23 00:13:19 +0800

    fix http request issue

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '95424a5ec2fe6beef5c374d2cd11b070';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
